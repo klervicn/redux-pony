bf105587d555101d5603f781af7e8ba67b511f42
Klervi Corbel <corbel.klervi@gmail.com>
2017-12-27 20:58:21 +0100

    Create components files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Ponies from './ponies/ponies';
+import InputPony from './ponies/inputPony';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <InputPony />
+        <Ponies />
+      </div>
+    );
   }
 }
 
