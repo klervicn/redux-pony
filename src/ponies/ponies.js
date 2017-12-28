d1824813a9955223720bb780141b2b84a278264a
Klervi Corbel <corbel.klervi@gmail.com>
2017-12-28 11:03:00 +0100

    Transform pony into presentationnal component
    - Correct ponies proptypes

--- src/ponies/ponies.js
+++ src/ponies/ponies.js
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 /* gere le delete et l'update des pony ? */
 
 export const Ponies = ({ poniesList }) => (
-  <ul>{poniesList.map(pony => <li>{pony.name}</li>)}</ul>
+  <ul>{poniesList.map(pony => <li key={pony.id}>{pony.name}</li>)}</ul>
 );
 
-
-
 Ponies.proptypes = {
-    poniesList = PropTypes.arrayOf(PropTypes.shape({
-        id : PropTypes.number.isRequired,
-        text : PropTypes.string.isRequired
-    }
-        /** Ajouter onDeleteClick, onUpdateClick */
-    ))
-}
+  poniesList: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired
+      }
+      /** Ajouter onDeleteClick, onUpdateClick */
+    )
+  )
+};
--- src/pony/pony.js
+++ src/pony/pony.js
@@ -2,14 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InputPonies from '../inputPony/inputPony';
 
-// component react ? need une input value donc surement
+export default class Pony extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    };
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.deletePony = this.deletePony.bind(this);
+  }
 
-export const Pony = ({ onDeleteClick, onUpdateClick, text, id }) => (
-  <li>
-    <input type="text" value={text} />
-    <button onClick={onDeleteClick}>Delete</button>
-  </li>
-);
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  deletePony(evt) {
+    evt.preventDefault();
+    this.props.onDeleteClick(this.props.id);
+  }
+
+  updatePony(evt) {
+    evt.preventDefault();
+    this.props.onUpdate(this.props.id, this.state.inputValue);
+  }
+
+  render() {
+    <div>
+      <li>
+        <input type="text" value={text} onChange={this.updateInputValue} />
+        <button onClick={this.updatePony}>Update</button>
+        <button onClick={this.deletePony}>Delete</button>
+      </li>
+    </div>;
+  }
+}
 
 InputPonies.propTypes = {
   onDeleteClick: PropTypes.func.isRequired,
