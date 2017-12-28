af7c50527294c4cc39a2d0a0c4ce2efa9fdf23b9
Klervi Corbel <corbel.klervi@gmail.com>
2017-12-28 20:56:37 +0100

    Add mock file

---
+++ src/mock.js
@@ -0,0 +1,40 @@
+export const ponies = {
+  data: [
+    {
+      id: 1,
+      name: 'Super Pony'
+    },
+    {
+      id: 2,
+      name: 'My Little Pony'
+    },
+    {
+      id: 3,
+      name: 'This is a Pony'
+    },
+    {
+      id: 4,
+      name: 'New Pony'
+    },
+    {
+      id: 5,
+      name: 'Another Pony'
+    },
+    {
+      id: 6,
+      name: 'A Pony'
+    },
+    {
+      id: 7,
+      name: 'New New Pony'
+    },
+    {
+      id: 8,
+      name: 'Mega Pony'
+    },
+    {
+      id: 9,
+      name: 'Hello Pony'
+    }
+  ]
+};
